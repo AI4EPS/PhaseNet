e402f5ec2505ec980b0cc59c44d71699c4b97ae4
Weiqiang Zhu <zhuwq0@gmail.com>
2025-04-09 17:27:09 -0700

    change folder level

--- tests/forge/forge.py
+++ tests/forge/forge.py
@@ -1,5 +1,5 @@
 import os
 
-cmd = f"python ../phasenet/predict.py --model=../model/190703-214543 --data_dir=./ --data_list=data.lst --format=das --amplitude --sampling_rate=1333.3333333333333 --result_dir=results --batch_size=1 --subdir=0"
+cmd = f"python ../../phasenet/predict.py --model=../../model/190703-214543 --data_dir=./ --data_list=data.lst --format=das --amplitude --sampling_rate=1333.3333333333333 --result_dir=results --batch_size=1 --subdir=0"
 
 os.system(cmd)
